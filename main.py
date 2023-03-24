04fec092b630c13d06ca6eef77b5fb2028637770
EduardoNunes21 <bkr778@gmail.com>
2023-03-23 21:25:49 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,130 @@
+from flask import Flask, render_template, redirect, request, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'helloworld'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passwords.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class passwords(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    login = db.Column("login", db.String(100))
+    password = db.Column("password", db.String(100))
+
+    def __init__(self, name, login, password):
+        self.name = name
+        self.login = login
+        self.password = password
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    password_to_delete = passwords.query.get_or_404(id)
+    try:
+        db.session.delete(password_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "Não foi possivel deletar"
+
+
+@app.route("/", methods=["POST", "GET"])
+@login_required
+
+
+def index():
+    if request.method == "POST":
+        name = request.form["nm"]
+        login = request.form["lg"]
+        password = request.form["pas"]
+        usr = passwords(name, login, password)
+        db.session.add(usr)
+        db.session.commit()
+
+    return render_template('index.html', values=passwords.query.all())
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('index'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(username) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(username=username, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('index'))
+
+    return render_template("register.html", user=current_user)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(host="127.0.0.1", port=8080, debug=True)
